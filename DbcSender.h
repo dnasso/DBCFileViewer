55864ebb6dd8a7e8b8884ccb3fa19e3d8f8bd3ac
dnasso <157742950+dnasso@users.noreply.github.com>
2025-09-16 22:39:26 -0500

    Modified the backend. Untested.
    It should be a step forward. But its not quite there. Mainly, I'm not super sure how to handle the sending/receiving of messages. So this is both non-blocking and pseudo-blocking. Additionally it just writes received messages to console. Not sure I used connect correctly.

--- DbcSender.h
+++ DbcSender.h
@@ -1,13 +1,17 @@
 #ifndef DBCSENDER_H
 #define DBCSENDER_H
 #include <QObject>
+#include <QTcpSocket>
 
 class DbcSender : public QObject {
 
 public:
-    Q_INVOKABLE void initiateConenction(QString Address, QString Port);
-    Q_INVOKABLE void sendCANMessage(QString CANid, QString canFrame, int rate, QString bus);
+    Q_INVOKABLE qint8 initiateConenction(QString Address, QString Port);
+    Q_INVOKABLE qint8 sendCANMessage(QString message);
     Q_INVOKABLE void stopCANMessage(QString CANid);
     Q_INVOKABLE void pauseCANMessage(QString CANid);
+
+private:
+    QTcpSocket socket;
 };
 #endif // DBCSENDER_H
